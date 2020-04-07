803993e506286dba03561281b8e18aa072fc8af8
PittUnkauf <61456078+PittUnkauf@users.noreply.github.com>
2020-04-07 15:36:08 +0200

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["ALPACA_API_KEY"] = "PK9E692IKJWE3BW2VZUX"
-os.environ["ALPACA_API_SECRET_KEY"] = "AlkTWFKlVyegBCTMu2j7ymTsLBSIvHfraKZGOKYq"
-os.environ["ALPHA_VANTAGE_KEY"] = "305EEPEIVGOAR6UT"
