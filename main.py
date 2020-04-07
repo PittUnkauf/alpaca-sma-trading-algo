3b1ec7031ce2795d7edf3ef435ad0a972e3626e8
PittUnkauf <61456078+PittUnkauf@users.noreply.github.com>
2020-04-07 17:38:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ ti = TechIndicators(key=ALPHA_VANTAGE_KEY, output_format='pandas')
 ts = TimeSeries(ALPHA_VANTAGE_KEY, output_format='pandas')
 
 while(True):
-    current_sma = ti.get_sma('IBM', interval='1min', time_period=30)[0].tail(2)['SMA'][1]
-    last_sma = ti.get_sma('IBM', interval='1min', time_period=30)[0].tail(2)['SMA'][0]
-    price = ts.get_intraday(symbol='AAPL', interval='1min')[0].tail(1)['4. close'][0]
+    current_sma = ti.get_sma(symbol='IBM', interval='1min', time_period=30)[0].tail(2)['SMA'][1]
+    last_sma = ti.get_sma(symobol='IBM', interval='1min', time_period=30)[0].tail(2)['SMA'][0]
+    price = ts.get_intraday(symbol='IBM', interval='1min')[0].tail(1)['4. close'][0]
 
     print(price)
 
